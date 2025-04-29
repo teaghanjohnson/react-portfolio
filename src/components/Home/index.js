2c983fd34817b80b52012907d1936ef3a297320b
teaghanjohnson <mylesjohnson@gmail.com>
2025-04-29 19:57:54 -0400

    Added animations to the home page built out text using array and for loop to add animations to each letter

---
+++ src/components/Home/index.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react';
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters letterClass={letterClass}
+            strArray={nameArray}
+            idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass}
+            strArray={jobArray}
+            idx={22} />
+
+
+        </h1>
+        <h2> Frontend Developer / Computer Science Major</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+
+    </div >
+  );
+}
+
+
+export default Home
